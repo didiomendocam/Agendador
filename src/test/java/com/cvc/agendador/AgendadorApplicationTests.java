b4250b55eb3a418007deb5f9260187d6353ce270
marco.mendonca <marco.mendonca@primesquad.com.br>
2023-01-14 15:43:49 -0300

    init commit projet

---
+++ src/test/java/com/cvc/agendador/AgendadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvc.agendador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
