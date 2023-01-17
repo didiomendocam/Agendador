76f98cf324d7cf454877d2cc5c557e6c4bcdf863
didiomendocam <111984411+didiomendocam@users.noreply.github.com>
2023-01-17 10:26:11 -0300

    Create RateRepository.java

---
+++ src/main/java/com/cvc/agendador/repository/RateRepository.java
@@ -0,0 +1,8 @@
+package com.cvc.agendador.repository;
+
+import com.cvc.agendador.model.Rate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RateRepository extends JpaRepository<Rate, Long> {
+
+}
