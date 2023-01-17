a735765be1d01424cb2111801a57b3cf7546d1da
didiomendocam <111984411+didiomendocam@users.noreply.github.com>
2023-01-17 10:26:40 -0300

    Update AccountService.java

--- src/main/java/com/cvc/agendador/service/AccountService.java
+++ src/main/java/com/cvc/agendador/service/AccountService.java
@@ -1,4 +1,27 @@
 package com.cvc.agendador.service;
 
+import com.cvc.agendador.model.Account;
+import com.cvc.agendador.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
+
 public class AccountService {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    public Account saveAccount(@RequestBody Account account) {
+        return accountRepository.save(account);
+    }
+
+    public Optional<Account> getAccountById(Long accountID) {
+        return accountRepository.findById(accountID);
+    }
+
+    public List<Account> getAccounts() {
+        return accountRepository.findAll();
+    }
 }
